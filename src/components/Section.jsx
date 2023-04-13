334cf09360f5c06f7081f32e996818e7f790453b
Konrad Jachimstal <k.jachimstal@gmail.com>
2023-04-13 19:37:40 +0200

    Refactored class Section to hook

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,12 +1,8 @@
-import React, { Component } from 'react';
-
-export class Section extends Component {
-  render() {
-    return (
-      <div className="container">
-        <h1> {this.props.title}</h1>
-        {this.props.children}
-      </div>
-    );
-  }
-}
+export const Section = ({ title, children }) => {
+  return (
+    <div className="container">
+      <h1> {title}</h1>
+      {children}
+    </div>
+  );
+};
