97d2b2a9a6966343d7c1b857bc9a22edb52974af
Konrad Jachimstal <k.jachimstal@gmail.com>
2023-04-13 19:35:32 +0200

    Refactored class FeedbackOptions to hook

--- src/components/FeedbackOptions.jsx
+++ src/components/FeedbackOptions.jsx
@@ -1,31 +1,27 @@
-import { Component } from 'react';
-
-export class FeedbackOptions extends Component {
-  render() {
-    return (
-      <div className="buttons">
-        <button
-          onClick={event => this.props.updateState(event.target.id)}
-          className="button"
-          id="good"
-        >
-          Good
-        </button>
-        <button
-          onClick={event => this.props.updateState(event.target.id)}
-          className="button"
-          id="neutral"
-        >
-          Neutral
-        </button>
-        <button
-          onClick={event => this.props.updateState(event.target.id)}
-          className="button"
-          id="bad"
-        >
-          Bad
-        </button>
-      </div>
-    );
-  }
-}
+export const FeedbackOptions = ({ updateState }) => {
+  return (
+    <div className="buttons">
+      <button
+        onClick={event => updateState(event.target.id)}
+        className="button"
+        id="good"
+      >
+        Good
+      </button>
+      <button
+        onClick={event => updateState(event.target.id)}
+        className="button"
+        id="neutral"
+      >
+        Neutral
+      </button>
+      <button
+        onClick={event => updateState(event.target.id)}
+        className="button"
+        id="bad"
+      >
+        Bad
+      </button>
+    </div>
+  );
+};
