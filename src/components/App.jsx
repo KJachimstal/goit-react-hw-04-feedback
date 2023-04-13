160d8a26edafec8552129347e28ce8f4edbec434
Konrad Jachimstal <k.jachimstal@gmail.com>
2023-04-13 19:32:08 +0200

    Refactored class App to hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,76 +1,67 @@
 import { Section } from './Section';
-import { Component } from 'react';
 import { Statistics } from './Statistics';
 import { FeedbackOptions } from './FeedbackOptions';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+import { useState } from 'react';
+
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNautral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const countTotalFeedback = () => {
+    return bad + neutral + good;
   };
 
-  countTotalFeedback = () => {
-    return this.state.bad + this.state.neutral + this.state.good;
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    const totalFeedback = this.countTotalFeedback();
+  const countPositiveFeedbackPercentage = () => {
+    const totalFeedback = countTotalFeedback();
     if (totalFeedback === 0) {
       return 0;
     }
 
-    return Math.floor((this.state.good / totalFeedback) * 100);
+    return Math.floor((good / totalFeedback) * 100);
   };
 
-  updateState = value => {
+  const updateState = value => {
     switch (value) {
       case 'good':
-        this.setState(state => ({
-          good: state.good + 1,
-        }));
+        setGood(good + 1);
         break;
       case 'neutral':
-        this.setState(state => ({
-          neutral: state.neutral + 1,
-        }));
+        setNautral(neutral + 1);
         break;
       case 'bad':
-        this.setState(state => ({
-          bad: state.bad + 1,
-        }));
+        setBad(bad + 1);
         break;
       default:
         console.log(`Update State eroor: ${value}`);
     }
   };
 
-  render() {
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          marginLeft: '50px',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <Section title="Please leave feedback">
-          <FeedbackOptions updateState={this.updateState} />
-        </Section>
-        <Section title="Statistics">
-          <Statistics
-            good={this.state.good}
-            neutral={this.state.neutral}
-            bad={this.state.bad}
-            total={this.countTotalFeedback}
-            positivePercentage={this.countPositiveFeedbackPercentage}
-          />
-        </Section>
-      </div>
-    );
-  }
-}
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        marginLeft: '50px',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <Section title="Please leave feedback">
+        <FeedbackOptions updateState={updateState} />
+      </Section>
+      <Section title="Statistics">
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={countTotalFeedback}
+          positivePercentage={countPositiveFeedbackPercentage}
+        />
+      </Section>
+    </div>
+  );
+};
