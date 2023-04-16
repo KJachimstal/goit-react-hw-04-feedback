7df5a86849aacbbddab0edc336bf8144458787a6
Konrad Jachimstal <k.jachimstal@gmail.com>
2023-04-16 19:02:14 +0200

    Refactored Statistics class to react hook

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,21 +1,24 @@
-import { Component } from 'react';
 import { Notification } from './Notification';
 
-export class Statistics extends Component {
-  render() {
-    if (this.props.total() === 0) {
-      return <Notification message="There is no feedback" />;
-    }
-    return (
-      <div className="statistics">
-        <span className="statistic">Good: {this.props.good}</span>
-        <span className="statistic">Neutral: {this.props.neutral}</span>
-        <span className="statistic">Bad: {this.props.bad}</span>
-        <span className="statistic">Total: {this.props.total()}</span>
-        <span className="statistic">
-          Positive feedback: {this.props.positivePercentage()}%
-        </span>
-      </div>
-    );
+export const Statistics = ({
+  good,
+  neutral,
+  bad,
+  total,
+  positivePercentage,
+}) => {
+  if (total() === 0) {
+    return <Notification message="There is no feedback" />;
   }
-}
+  return (
+    <div className="statistics">
+      <span className="statistic">Good: {good}</span>
+      <span className="statistic">Neutral: {neutral}</span>
+      <span className="statistic">Bad: {bad}</span>
+      <span className="statistic">Total: {total()}</span>
+      <span className="statistic">
+        Positive feedback: {positivePercentage()}%
+      </span>
+    </div>
+  );
+};
